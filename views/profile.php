573824b0d95ca785537cfa53bad0412b0bc661fc
Diana <Diana@DiananNadav>
2015-06-18 22:59:25 +0300

    h

--- views/profile.php
+++ views/profile.php
@@ -1,5 +1,12 @@
 <div class="container" ng-controller="profileCtrl">
     <?php
+    $con = mysqli_connect("localhost", "root", "", "Group7");
+      if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        else {
+            echo "connected.<br/>";
+    }
     (isset($_POST['fname'])) ? $fname = filter_var ($_POST['fname'],FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH) : $fname = "Profile";    
     (isset($_POST['lname'])) ? $lname = " " . $_POST['lname'] : $lname = " ";
     (isset($_POST['age'])) ? $age = $_POST['age'] : $age = "";
@@ -24,7 +31,6 @@
             $image = "views/upload/uploadProfilePics/" . $_FILES["img"]["name"];
         }
     }
-    $alternative = "images/permissionDenied.png";
     ?>
     <div class="capitalize"> <h1><?php echo $fname . $lname ?></h1>
     </div>
@@ -32,67 +38,48 @@
         <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-4">
-            <img id="profileImg" src="
-            <?php
-            if ($username != "")
-                echo $image;
-            else
-                echo $alternative;
-            ?>" alt="Profile Picture" >
+            <img id="profileImg" src="<?php echo $image;?>">
             </div>
         </div>
         <div class="row">
             <div class="col-lg-4"></div>
             <div class="col-md-4">
-            <a href="index.php?action=registration"><?php if ($username == "") echo  "<strong> Register </strong>" ; ?> </a>
-            <?php if ($username == "")  echo "for Permission"; ?>
             </div>
         </div>
         <form class="form-horizontal">
             <div class="form-group">
-                <label class="col-sm-2 control-label">
-                    <?php
-                    if ($username != "")
-                        echo "User Name"
-                        ?></label>
+                <label class="col-sm-2 control-label">User Name</label>
                 <div class="col-sm-10">
                     <div class="form-control-static "><?php echo $username ?></div>
                 </div>
-                <label class="col-sm-2 control-label">
-                    <?php
-                    if ($username != "")
-                        echo "Password"
-                        ?></label>
+                <label class="col-sm-2 control-label">Password</label>
                 <div class="col-sm-10">
                     <div class="form-control-static "><?php echo $password ?></div>
                 </div>
-                <label class="col-sm-2 control-label">
-                    <?php if ($username != "") echo "Email" ?></label>
+                <label class="col-sm-2 control-label">Email</label>
                 <div class="col-sm-10">
                     <div class="form-control-static "><?php echo $email ?></div>
                 </div>
-                <label class="col-sm-2 control-label">
-                    <?php
-                    if ($username != "")
-                        echo "Uploads"
-                        ?></label>
+                <label class="col-sm-2 control-label"/>
+                   Uploads
                 <div class="col-sm-10">
                     <div class="form-control-static "> 
-                        <?php if ($username != "")
-                            echo "<ol>";
-                        else
-                            echo "<ul>";
-                        ?>
-                        <li> <a href=""><?php if ($username != "") echo " Algebra-Dimension" ?> </a></li>
-                        <li> <a href=""><?php if ($username != "") echo " Computer-Science-SUSU PROBLEM" ?> </a></li>
-                        <li> <a href=""><?php if ($username != "") echo " Calculus Integral" ?> </a></div></li>
-                    <?php
-                    if ($username != "")
-                        echo "</ol>";
-                    else
-                        echo "</ul>";
-                    ?>                         
+                        <ol>
+                        <li> <a href=""> Algebra-Dimension </a></li>
+                        <li> <a href=""> Computer-Science-SUSU PROBLEM</a></li>
+                        <li> <a href=""> Calculus Integral </a></div></li>
+                        </ol>
                 </div>
             </div>
     </div>
 </div>
+
+<?php   $insertSql = "INSERT INTO log-in (username, password) "
+                    . "VALUES ('$username', '$password')";
+	if (!mysqli_query($con, $insertSql)) {
+            die('Error: ' . mysqli_error($con));
+	}
+	echo "1 record added<br><br>";
+
+	
+	mysqli_close($con); ?>
